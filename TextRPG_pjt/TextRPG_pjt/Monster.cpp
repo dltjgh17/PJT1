230631ec2a117f7461b28964c0ab79d4e5462bfa
oSiris1o7 <siuesen@gmail.com>
2025-06-20 13:33:56 +0900

    update - Monster.cpp / add - Monster.h / add - monsterData.h

---
+++ TextRPG_pjt/TextRPG_pjt/Monster.cpp
@@ -0,0 +1,39 @@
+﻿//Monster.cpp
+#include "Monster.h"
+#include <iostream>
+Monster::Monster(const std::string& name, int health, int attack) : name_(name), health_(health), attack_(attack)
+{
+
+}
+
+
+std::string Monster::GetName() const
+{
+	return name_;
+}
+int Monster::GetHealth() const
+{
+	return health_;
+}
+int Monster::GetAttack() const
+{
+	return attack_;
+}
+
+void Monster::TakeDamage(int damage)
+{
+	health_ -= damage;
+	if (health_ < 0)
+	{
+		health_ = 0;
+        
+	}
+
+	//로그 처리 필요한지?
+}
+
+
+void Monster::SetHealth(int playerLevel)
+{
+	health_ *= playerLevel;
+}
---
+++ TextRPG_pjt/TextRPG_pjt/MonsterData.h
@@ -0,0 +1,43 @@
+﻿//MonsterData.h
+
+#ifndef MonsterData_h
+
+#include <string>
+#include <map>
+#include <memory>
+#include "Monster.h"
+
+
+struct MonsterStats
+{
+	std::string name;
+	int health;
+	int attack;
+};
+
+
+std::unique_ptr<Monster> CreateMonster(int monsterId)
+{
+	static const std::map<int, MonsterStats> monsterTamplates =
+	{
+		{1,{"Slime",50,5}},
+		{2,{"Slime",50,5}},
+		{3,{"Slime",50,5}}
+	};
+
+
+	auto it = monsterTamplates.find(monsterId);
+	if (it != monsterTamplates.end())
+	{
+		const MonsterStats& stats = it->second;
+		return std::make_unique<Monster>(stats.name, stats.health, stats.attack);
+	}
+	else
+	{
+		//못찾은 경우?
+	}
+}
+
+
+
+#endif // !MonsterData_h
