d0f39e25504445fb86909d925c2375752bf15373
LeeSh-213 <hasanghyun66@naver.com>
2025-06-23 09:14:00 +0900

    6.23 배틀 시스템 수정사항 : 병합 중 충돌 내역 fix

--- TextRPG_pjt/TextRPG_pjt/BattleSystem.cpp
+++ TextRPG_pjt/TextRPG_pjt/BattleSystem.cpp
@@ -1,14 +1,7 @@
 #include "BattleSystem.h"
 #include "Character.h"
-<<<<<<< HEAD
-
-
 #include <iostream>
-=======
 #include "MonsterData.h"
-#include "Monster.h"
-
->>>>>>> 57e491f46ad5467c820e2fd0eba0210f317522fa
 #include <cstdlib>
 #include <ctime>
 #include <thread>
@@ -22,35 +15,26 @@ using namespace std;
 Monster* BattleSystem::CreateRandomEnemy()
 {
     srand(static_cast<unsigned int>(time(nullptr)));
-    int r = rand() % 4;
+    int r = rand() % 3;
 
     switch (r)
     {
     case 0: return new Monster("Slime", 20, 5);
 	case 1: return new Monster("Goblin", 15, 7);
 	case 2: return new Monster("Golem", 30, 10);
-	case 3: return new Monster("Dragon", 45, 15);
     }
 
 }
 
-<<<<<<< HEAD
-void BattleSystem::StartBattle(Character* Player)
-=======
+
 void BattleSystem::StartBattle(Character* player)
->>>>>>> 57e491f46ad5467c820e2fd0eba0210f317522fa
 {
     Monster* monster = CreateRandomEnemy();  //ToDo:
 
-<<<<<<< HEAD
-    cout << "\n Battle Start!" << Player->getName() << "vs" << enemy->GetName() << endl;  // ToDo:
 
-    while (Player->getHp() > 0 && enemy->getHp() > 0)
-=======
     cout << "\n Battle Start!" << player->getName() << "vs" << monster->GetName() << endl;  // ToDo:
 
     while (player->getHealth() > 0 && monster->GetHealth() > 0)
->>>>>>> 57e491f46ad5467c820e2fd0eba0210f317522fa
     {
         monster->TakeDamage(player->getAttack());    //ToDo:
 
